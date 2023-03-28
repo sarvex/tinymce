0763c18637c191d1404e2ef967167e50b87c6270
Mike Zhu <mkzh.org@gmail.com>
2023-03-28 14:33:00 +1000

    TINY-9453: Resolve failures in EnterKeyListsTest

--- modules/tinymce/src/core/test/ts/browser/keyboard/EnterKeyListsTest.ts
+++ modules/tinymce/src/core/test/ts/browser/keyboard/EnterKeyListsTest.ts
@@ -1,10 +1,9 @@
-import { Keys } from '@ephox/agar';
+import { Keys, RealKeys } from '@ephox/agar';
 import { describe, it } from '@ephox/bedrock-client';
 import { LegacyUnit, TinyAssertions, TinyContentActions, TinyHooks, TinySelections } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
-import Tools from 'tinymce/core/api/util/Tools';
 
 describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
   const hook = TinyHooks.bddSetupLight<Editor>({
@@ -18,78 +17,75 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     base_url: '/project/tinymce/js/tinymce'
   }, []);
 
-  const pressEnter = (editor: Editor, evt?: any) => {
-    const dom = editor.dom;
-    const target = editor.selection.getNode();
-
-    evt = Tools.extend({ keyCode: 13, shiftKey: false }, evt);
-
-    dom.dispatch(target, 'keydown', evt);
-    dom.dispatch(target, 'keypress', evt);
-    dom.dispatch(target, 'keyup', evt);
+  const pressEnter = async (modifier?: 'shift' | 'ctrl') => {
+    if (modifier) {
+      await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.combo(modifier === 'shift' ? { shiftKey: true } : { ctrlKey: true }, 'Enter') ]);
+    } else {
+      await RealKeys.pSendKeysOn('iframe => body', [ RealKeys.text('Enter') ]);
+    }
   };
 
   const trimBrsOnIE = (html: string) => {
     return html.replace(/<br[^>]*>/gi, '');
   };
 
-  it('Enter inside empty li in beginning of ol', () => {
+  it('Enter inside empty li in beginning of ol', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><br></li><li>a</li></ol>';
     LegacyUnit.setSelection(editor, 'li', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<p>\u00a0</p><ol><li>a</li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Enter inside empty li at the end of ol', () => {
+  it('Enter inside empty li at the end of ol', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li>a</li><li><br></li></ol>';
     LegacyUnit.setSelection(editor, 'li:last-of-type', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<ol><li>a</li></ol><p>\u00a0</p>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Shift+Enter inside empty li in the middle of ol', () => {
+  it('Shift+Enter inside empty li in the middle of ol', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li>a</li><li><br></li><li>b</li></ol>';
     editor.selection.setCursorLocation(editor.dom.select('li:nth-child(2)')[0], 0);
-    pressEnter(editor, { shiftKey: true });
+    await pressEnter('shift');
     TinyAssertions.assertRawContent(editor, '<ol><li>a</li><li><br><br></li><li>b</li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Shift+Enter inside empty li in beginning of ol', () => {
+  it('Shift+Enter inside empty li in beginning of ol', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><br></li><li>a</li></ol>';
     editor.selection.setCursorLocation(editor.dom.select('li')[0], 0);
-    pressEnter(editor, { shiftKey: true });
+    await pressEnter('shift');
     TinyAssertions.assertRawContent(editor, '<ol><li><br><br></li><li>a</li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Shift+Enter inside empty li at the end of ol', () => {
+  it('Shift+Enter inside empty li at the end of ol', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li>a</li><li><br></li></ol>';
     editor.selection.setCursorLocation(editor.dom.select('li')[1], 0);
-    pressEnter(editor, { shiftKey: true });
+    await pressEnter('shift');
     TinyAssertions.assertRawContent(editor, '<ol><li>a</li><li><br><br></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter inside empty li in the middle of ol', () => {
+  it('Enter inside empty li in the middle of ol', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li>a</li><li><br></li><li>b</li></ol>';
     LegacyUnit.setSelection(editor, 'li:nth-child(2)', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<ol><li>a</li></ol><p>\u00a0</p><ol><li>b</li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
   // Nested lists in LI elements
 
-  it('Enter inside empty LI in beginning of OL in LI', () => {
+  it('Enter inside empty LI in beginning of OL in LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -104,7 +100,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'li li', 0);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -120,7 +116,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter inside empty LI in middle of OL in LI', () => {
+  it('Enter inside empty LI in middle of OL in LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -136,7 +132,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'li li:nth-child(2)', 0);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -156,7 +152,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter inside empty LI in end of OL in LI', () => {
+  it('Enter inside empty LI in end of OL in LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -171,7 +167,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'li li:last-of-type', 0);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -189,7 +185,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
   // Nested lists in OL elements
 
-  it('Enter before nested list', () => {
+  it('Enter before nested list', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -204,7 +200,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'ol > li', 1);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -221,7 +217,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter inside empty LI in beginning of OL in OL', () => {
+  it('Enter inside empty LI in beginning of OL in OL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -235,7 +231,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'ol ol li', 0);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -250,7 +246,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter inside empty LI in middle of OL in OL', () => {
+  it('Enter inside empty LI in middle of OL in OL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -265,7 +261,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'ol ol li:nth-child(2)', 0);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -283,7 +279,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter inside empty LI in end of OL in OL', () => {
+  it('Enter inside empty LI in end of OL in OL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = trimBrsOnIE(
       '<ol>' +
@@ -297,7 +293,7 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
 
     LegacyUnit.setSelection(editor, 'ol ol li:last-of-type', 0);
     editor.focus();
-    pressEnter(editor);
+    await pressEnter();
 
     TinyAssertions.assertContent(editor,
       '<ol>' +
@@ -312,128 +308,129 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Enter at beginning of first DT inside DL', () => {
+  it('Enter at beginning of first DT inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dt>a</dt></dl>';
     LegacyUnit.setSelection(editor, 'dt', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dt>\u00a0</dt><dt>a</dt></dl>');
     assert.equal(editor.selection.getNode().nodeName, 'DT');
   });
 
-  it('Enter at beginning of first DD inside DL', () => {
+  it('Enter at beginning of first DD inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dd>a</dd></dl>';
     LegacyUnit.setSelection(editor, 'dd', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dd>\u00a0</dd><dd>a</dd></dl>');
     assert.equal(editor.selection.getNode().nodeName, 'DD');
   });
 
-  it('Enter at beginning of middle DT inside DL', () => {
+  it('Enter at beginning of middle DT inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dt>a</dt><dt>b</dt><dt>c</dt></dl>';
     LegacyUnit.setSelection(editor, 'dt:nth-child(2)', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dt>a</dt><dt>\u00a0</dt><dt>b</dt><dt>c</dt></dl>');
     assert.equal(editor.selection.getNode().nodeName, 'DT');
   });
 
-  it('Enter at beginning of middle DD inside DL', () => {
+  it('Enter at beginning of middle DD inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dd>a</dd><dd>b</dd><dd>c</dd></dl>';
     LegacyUnit.setSelection(editor, 'dd:nth-child(2)', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dd>a</dd><dd>\u00a0</dd><dd>b</dd><dd>c</dd></dl>');
     assert.equal(editor.selection.getNode().nodeName, 'DD');
   });
 
-  it('Enter at end of last DT inside DL', () => {
+  it('Enter at end of last DT inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dt>a</dt></dl>';
     LegacyUnit.setSelection(editor, 'dt', 1);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dt>a</dt><dt>\u00a0</dt></dl>');
     assert.equal(editor.selection.getNode().nodeName, 'DT');
   });
 
-  it('Enter at end of last DD inside DL', () => {
+  it('Enter at end of last DD inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dd>a</dd></dl>';
     LegacyUnit.setSelection(editor, 'dd', 1);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dd>a</dd><dd>\u00a0</dd></dl>');
     assert.equal(editor.selection.getNode().nodeName, 'DD');
   });
 
-  it('Enter at end of last empty DT inside DL', () => {
+  it('Enter at end of last empty DT inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dt>a</dt><dt></dt></dl>';
     LegacyUnit.setSelection(editor, 'dt:nth-child(2)', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dt>a</dt></dl><p>\u00a0</p>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Enter at end of last empty DD inside DL', () => {
+  it('Enter at end of last empty DD inside DL', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<dl><dd>a</dd><dd></dd></dl>';
     LegacyUnit.setSelection(editor, 'dd:nth-child(2)', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<dl><dd>a</dd></dl><p>\u00a0</p>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Enter at beginning of P inside LI', () => {
+  it('Enter at beginning of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
     LegacyUnit.setSelection(editor, 'p', 0);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<ol><li>\u00a0</li><li><p>abcd</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Enter inside middle of P inside LI', () => {
+  it('Enter inside middle of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
     LegacyUnit.setSelection(editor, 'p', 2);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<ol><li><p>ab</p></li><li><p>cd</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Enter at end of P inside LI', () => {
+  it('Enter at end of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
     LegacyUnit.setSelection(editor, 'p', 4);
-    pressEnter(editor);
+    await pressEnter();
     TinyAssertions.assertContent(editor, '<ol><li><p>abcd</p></li><li>\u00a0</li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'LI');
   });
 
-  it('Shift+Enter at beginning of P inside LI', () => {
+  it('Shift+Enter at beginning of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
-    LegacyUnit.setSelection(editor, 'p', 0);
-    pressEnter(editor, { shiftKey: true });
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0 ], 0);
+    await pressEnter('shift');
     TinyAssertions.assertContent(editor, '<ol><li><p><br>abcd</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
+    TinyAssertions.assertContent(editor, 'w');
   });
 
-  it('Shift+Enter inside middle of P inside LI', () => {
+  it('Shift+Enter inside middle of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
-    LegacyUnit.setSelection(editor, 'p', 2);
-    pressEnter(editor, { shiftKey: true });
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0 ], 2);
+    await pressEnter('shift');
     TinyAssertions.assertContent(editor, '<ol><li><p>ab<br>cd</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Shift+Enter at end of P inside LI', () => {
+  it('Shift+Enter at end of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
-    LegacyUnit.setSelection(editor, 'p', 4);
-    pressEnter(editor, { shiftKey: true });
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0 ], 4);
+    await pressEnter('shift');
     TinyAssertions.assertContent(
       editor,
       '<ol><li><p>abcd<br><br></p></li></ol>'
@@ -441,29 +438,29 @@ describe('browser.tinymce.core.keyboard.EnterKeyListsTest', () => {
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Ctrl+Enter at beginning of P inside LI', () => {
+  it('Ctrl+Enter at beginning of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
-    LegacyUnit.setSelection(editor, 'p', 0);
-    pressEnter(editor, { ctrlKey: true });
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0 ], 0);
+    await pressEnter('ctrl');
     TinyAssertions.assertContent(editor, '<ol><li><p>\u00a0</p><p>abcd</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Ctrl+Enter inside middle of P inside LI', () => {
+  it('Ctrl+Enter inside middle of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
-    LegacyUnit.setSelection(editor, 'p', 2);
-    pressEnter(editor, { ctrlKey: true });
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0 ], 2);
+    await pressEnter('ctrl');
     TinyAssertions.assertContent(editor, '<ol><li><p>ab</p><p>cd</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
 
-  it('Ctrl+Enter at end of P inside LI', () => {
+  it('Ctrl+Enter at end of P inside LI', async () => {
     const editor = hook.editor();
     editor.getBody().innerHTML = '<ol><li><p>abcd</p></li></ol>';
-    LegacyUnit.setSelection(editor, 'p', 4);
-    pressEnter(editor, { ctrlKey: true });
+    TinySelections.setCursor(editor, [ 0, 0, 0, 0 ], 4);
+    await pressEnter('ctrl');
     TinyAssertions.assertContent(editor, '<ol><li><p>abcd</p><p>\u00a0</p></li></ol>');
     assert.equal(editor.selection.getNode().nodeName, 'P');
   });
